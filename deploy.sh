96517d7a353c4adfca9fdc22f1cba8fa77315120
Mishra Saurabh <saaurabhmishra@gmail.com>
2025-09-16 12:50:28 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IMAGE_NAME="ghcr.io/${GHCR_USERNAME}/ezrto-landing:latest"
+CONTAINER_NAME="ezrto-landing"
+
+# Stop and remove old container
+docker rm -f $CONTAINER_NAME || true
+
+# Pull latest image
+docker pull $IMAGE_NAME
+
+# Run container with environment variables
+docker run -d \
+  --name $CONTAINER_NAME \
+  --network ezrto_network  \
+  -p 3001:3001 \
+  --restart unless-stopped \
+  $IMAGE_NAME
+
+echo "Deployment complete!"
+
