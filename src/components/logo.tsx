2aa6f5c59d734f3576556ea3177cc1d108229db3
Mishra Saurabh <saaurabhmishra@gmail.com>
2025-09-21 02:15:13 +0530

    Reduce logo width in AppLogo component
    Changed the logo container width from w-28 to w-24 in the AppLogo component to adjust the displayed logo size.

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function AppLogo(){
     return(
         <Link href="/" className="flex items-center space-x-2 cursor-pointer">
-            <div className="relative w-28 h-16">
+            <div className="relative w-24 h-12">
                 <Image
                     src="/logo.png"
                     alt="ezrto"
