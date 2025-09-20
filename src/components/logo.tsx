0e8395439029b1e3784be6fc2cf9dc9e09209aba
Mishra Saurabh <saaurabhmishra@gmail.com>
2025-09-21 02:08:37 +0530

    Create logo.tsx

---
+++ src/components/logo.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function AppLogo(){
+    return(
+        <Link href="/" className="flex items-center space-x-2 cursor-pointer">
+            <div className="relative w-28 h-16">
+                <Image
+                    src="/logo.png"
+                    alt="ezrto"
+                    fill
+                    sizes="100vw"
+                    priority
+                    className="object-contain"
+                />
+            </div>
+        </Link>
+    )
+}
